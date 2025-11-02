820ab457a5f3cc26e213181960f53f8743ccdf30
LIUDMILA KIK <tender.ird@gmail.com>
2025-11-02 19:34:43 +0100

    6.1

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,76 +1,60 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
-import { Toaster, toast } from 'react-hot-toast';
-import css from './NotesPage.module.css';
-import { fetchNotes } from '@/lib/api';
-import NoteList from '@/components/NoteList/NoteList';
-import SearchBox from '@/components/SearchBox/SearchBox';
 import Pagination from '@/components/Pagination/Pagination';
-import Modal from '@/components/Modal/Modal';
+import css from '../page.module.css';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useDebounce } from 'use-debounce';
+import SearchBox from '@/components/SearchBox/SearchBox';
 import NoteForm from '@/components/NoteForm/NoteForm';
-import Loading from '../loading';
-import Error from './error';
+import Modal from '@/components/Modal/Modal';
+import NoteList from '@/components/NoteList/NoteList';
+import ErrorMessage from '@/components/ErrorMessage/ErrorMessage';
+import Loader from '@/components/Loader/Loader';
+import { fetchNotes } from '@/lib/api';
 
 export default function NotesClient() {
-  const [search, setSearch] = useState<string>('');
   const [page, setPage] = useState<number>(1);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [request, setRequest] = useState<string>('');
+  const [debouncedRequest] = useDebounce(request, 800);
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
-  const perPage = 12;
+  const toggleModal = () => setOpenModal(!openModal);
 
-  const { data, error, isSuccess, isError, isLoading } = useQuery({
-    queryKey: ['notes', search, page],
-    queryFn: () => fetchNotes(search, page, perPage),
-    placeholderData: keepPreviousData,
+  const { data, isError, isFetching } = useQuery({
+    queryKey: ['notes', page, debouncedRequest],
+    queryFn: () => fetchNotes(page, debouncedRequest),
+    retry: false,
+    placeholderData: prev => prev,
   });
 
-  const totalPages = data?.totalPages ?? 0;
-  const totalNotes = data?.notes?.length ?? 0;
-
-  useEffect(() => {
-    if (isSuccess && totalNotes === 0) {
-      toast.error('No notes found for your request.', { duration: 1000 });
-    }
-  }, [isSuccess, totalNotes]);
-
-  const updateSearchQuery = useDebouncedCallback((value: string) => {
+  const onChangeRequest = (newRequest: string) => {
     setPage(1);
-    setSearch(value);
-  }, 400);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
+    setRequest(newRequest);
   };
 
   return (
     <div className={css.app}>
-      <Toaster />
       <div className={css.toolbar}>
-        <SearchBox value={search} onChange={updateSearchQuery} />
-        {isSuccess && totalPages > 1 && (
-          <Pagination page={page} totalPages={totalPages} setPage={setPage} />
+        <SearchBox onChange={onChangeRequest} />
+        {data && data.totalPages > 1 && (
+          <Pagination totalPages={data.totalPages} setPage={setPage} page={page} />
         )}
-        <button className={css.button} onClick={openModal}>
+        <button onClick={toggleModal} className={css.button}>
           Create note +
         </button>
       </div>
-      {isLoading && <Loading />}
-      {isError && (
-        <Error error={error} reset={() => fetchNotes(search, page, perPage)} />
-      )}
-      {data?.notes && totalNotes > 0 && <NoteList notes={data?.notes} />}
-      {isModalOpen && (
-        <Modal onClose={closeModal}>
-          <NoteForm onCancel={closeModal} />
+
+      {isFetching && <Loader />}
+      {isError && <ErrorMessage />}
+
+      {openModal && (
+        <Modal onClose={toggleModal}>
+          <NoteForm onClose={toggleModal} />
         </Modal>
       )}
+
+      {data && data.totalPages > 0 && <NoteList notes={data.notes} />}
     </div>
   );
 }
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,44 +1,17 @@
-// app/notes/page.tsx
+import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
 
-//import NoteList from '@/components/NoteList/NoteList';
-
-// const Notes = async () => {
-//   const response = await getNotes();
-
-//   return (
-//     <section>
-//       <h1>Notes List</h1>
-//       {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
-//     </section>
-//   );
-// };
-
-// export default Notes;
-
-import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from '@tanstack/react-query';
-import { fetchNotes } from '@/lib/api';
-import NotesClient from './Notes.client';
-
-const Notes = async () => {
+export default async function Notes() {
   const queryClient = new QueryClient();
 
-  const search = '';
-  const page = 1;
-
   await queryClient.prefetchQuery({
-    queryKey: ['notes'],
-    queryFn: () => fetchNotes(search, page),
+    queryKey: ["notes", 1, ""],
+    queryFn: () => fetchNotes(1, ""),
   });
-
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <NotesClient />
     </HydrationBoundary>
   );
-};
-
-export default Notes;
+}
